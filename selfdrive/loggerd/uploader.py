0b5db6e1941ca9cf8141863b7a50fe50fdbeb947
Eric Torgersen <etorgersen@albany.edu>
2019-06-04 08:47:20 -0400

    add 192.168.7. to hotspot check

--- selfdrive/loggerd/uploader.py
+++ selfdrive/loggerd/uploader.py
@@ -86,7 +86,8 @@ def is_on_hotspot():
 
     is_android = result.startswith('192.168.43.')
     is_ios = result.startswith('172.20.10.')
-    return (is_android or is_ios)
+    is_b4 = result.startswith('192.168.7.')
+    return (is_android or is_ios or is_b4)
   except:
     return False
 
